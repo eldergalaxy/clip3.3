dc6ae136c1d1a3f947cd8be7298b7311bd6a5019
eldergalaxy <173822183+eldergalaxy@users.noreply.github.com>
2026-01-05 17:03:23 -0600

    fixed diagonal walk anim

--- 2d-action-adventure/Player/player.gd
+++ 2d-action-adventure/Player/player.gd
@@ -18,16 +18,17 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.play("move_right")
 		
 		
-	if velocity.x < 0 :
+	elif  velocity.x < 0 :
 		$AnimatedSprite2D.play("move_left")
 		
-	if velocity.y < 0 :
+	elif velocity.y < 0 :
 		$AnimatedSprite2D.play("move_up")
 		
-	if velocity.y > 0 :
+	elif velocity.y > 0 :
 		$AnimatedSprite2D.play("move_down")
 		
-	if velocity == Vector2(0,0):
+	else: 
+		velocity == Vector2(0,0)
 		$AnimatedSprite2D.stop()
 		
 	
