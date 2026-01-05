8a9425570c96034427411dff5f1f435706072c72
eldergalaxy <173822183+eldergalaxy@users.noreply.github.com>
2026-01-05 15:15:13 -0600

    initial commit for clip3.3

---
+++ 2d-action-adventure/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		print("you're pressing right")
+	
